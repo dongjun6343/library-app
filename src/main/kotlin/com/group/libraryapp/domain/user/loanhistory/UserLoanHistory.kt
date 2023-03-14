923001d1bc5096850779268d5bdf843cbe1269ee
dongjun6343 <dongjun6343@naver.com>
2023-03-15 00:00:04 +0900

    UserLoanHistory - kotiln

---
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -0,0 +1,33 @@
+package com.group.libraryapp.domain.user.loanhistory
+
+import com.group.libraryapp.domain.user.User
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToMany
+import javax.persistence.ManyToOne
+
+
+@Entity
+class UserLoanHistory (
+
+        @ManyToOne
+        val user: User,
+
+        // val : 불변
+        val bookName: String,
+
+        // var : 가변
+        var isReturn: Boolean,
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+){
+
+    fun doReturn (){
+        this.isReturn = true
+    }
+}
