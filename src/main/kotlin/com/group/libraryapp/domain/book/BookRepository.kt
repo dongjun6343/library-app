a4b26139834a240bf834473208efa5b3ff813159
dongjun6343 <dongjun6343@naver.com>
2023-03-16 22:00:43 +0900

    Repository - kotlin으로 변경

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -0,0 +1,10 @@
+package com.group.libraryapp.domain.book
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+// 코틀린에서는 상속받는것을 : 로 한다.
+interface BookRepository : JpaRepository<Book, Long>{
+
+    fun findByName(bookName: String): Optional<Book>
+}
