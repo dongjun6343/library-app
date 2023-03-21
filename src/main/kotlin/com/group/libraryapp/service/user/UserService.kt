b22b4caf58c6fce6295537db30bc1bb6a033bb20
dongjun6343 <dongjun6343@naver.com>
2023-03-21 23:00:50 +0900

    확장함수를 이용해서 findByIdOrThrow 커스텀

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.util.fail
+import com.group.libraryapp.util.findByIdOrThrow
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -32,8 +33,9 @@ class UserService (
     @Transactional
     fun updateUserName(request: UserUpdateRequest){
         // 코틀린의 확장함수 사용해서 제어할 수 없을 거 같은 findById의 Optional 제거하기.
-        // findByIdOrNull
-        val user = userRepository.findByIdOrNull(request.id) ?: fail()
+        //  findById -> findByIdOrNull
+        // val user = userRepository.findByIdOrNull(request.id) ?: fail()
+        val user = userRepository.findByIdOrThrow(request.id)
         user.updateName(request.name)
     }
 
--- src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
+++ src/main/kotlin/com/group/libraryapp/util/ExceptionUtils.kt
@@ -1,5 +1,15 @@
 package com.group.libraryapp.util
 
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
 fun fail(): Nothing{
     throw IllegalArgumentException()
+}
+
+// null인 경우 exception을 발생하는 확장함수를 만든다
+// 따라서 ?를 쓰지 않는다.
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
+    // this -> CrudRepository를 가리킨다.
+    return this.findByIdOrNull(id) ?: fail()
 }
